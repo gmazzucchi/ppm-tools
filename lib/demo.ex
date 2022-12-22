4428cf0dcaa81740c7728002a41cf82f0c38fbca
tzarjakob <giacomo.mazzucchi@protonmail.com>
2022-12-22 19:49:08 +0100

    feat: second demo

--- lib/demo.ex
+++ lib/demo.ex
@@ -1,12 +1,11 @@
 defmodule Demo do
-
   def demo() do
     rows = 100
     cols = 200
 
     points =
-      for ir <- 0..rows-1 do
-        for ic <- 0..cols-1 do
+      for ir <- 0..(rows - 1) do
+        for ic <- 0..(cols - 1) do
           {ir, ic}
         end
       end
@@ -30,5 +29,48 @@ defmodule Demo do
       |> Map.put(12, :green)
 
     PpmTools.to_ppm("./output.ppm", content, rows, cols, color_map, :white, 4)
+  end
+
+  '''
+  Example taken from https://adventofcode.com/2022/day/22
+  '''
+
+  def demo2() do
+    map =
+      File.read!("./text.txt")
+      |> String.split("\n")
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.reduce({0, Map.new()}, fn crow, {irow, imap} ->
+        {irow + 1,
+         crow
+         |> Enum.reduce({0, imap}, fn elem, {icol, jmap} ->
+           {icol + 1,
+            case elem do
+              "." ->
+                Map.put(jmap, {irow, icol}, :free)
+
+              "#" ->
+                Map.put(jmap, {irow, icol}, :wall)
+
+              _ ->
+                jmap
+            end}
+         end)
+         |> elem(1)}
+      end)
+      |> elem(1)
+
+    xs = map |> Map.keys() |> Enum.map(fn {x, _y} -> x end) |> Enum.max() |> IO.inspect()
+    ys = map |> Map.keys() |> Enum.map(fn {_x, y} -> y end) |> Enum.max() |> IO.inspect()
+
+    PpmTools.to_ppm(
+      "./demo2.ppm",
+      map,
+      xs + 1,
+      ys + 1,
+      Map.new() |> Map.put(:wall, :black) |> Map.put(:free, :yellow),
+      :white,
+      20
+    )
   end
 end
--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ElixirPpm.MixProject do
       app: :elixir_ppm,
       version: "0.1.0",
       elixir: "~> 1.14",
-      start_permanent: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod
     ]
   end
 
